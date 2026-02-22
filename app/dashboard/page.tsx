95074eed6fe0f1a6fa2308d48dce8dd9f2625366
Imraan Mohamedhoesein <imraan.mohamedhoesein@gmail.com>
2026-02-22 22:27:18 +0000

    fallback if user is not logged in to go back to sign in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import { UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
 export default async function DashboardPage() {
   const user = await currentUser()
+  if (!user) {
+    redirect('/sign-in')
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-bold">Welcome to Nexus</h1>
